fafc16436311a1692d97f3b435b992d9d1f0430f
Talia Campbell <talia.campbell@csedge.org>
2018-10-06 15:45:47 -0400

    Completed Challenge 1
    Remember to console.log

--- data.js
+++ data.js
@@ -1,5 +1,32 @@
 // Example
-let message = "Hello World!";
-console.log(message);
 
-// Type your code below!
+// Type your code below
+
+let f = 1;
+let x = 3;
+let y = 7;
+console.log(x);
+console.log(y);
+console.log(f);
+
+let playSoccer = false;
+let isItRaining = false;
+let LikesClouds = true;
+console.log(playSoccer);
+console.log(isItRaining);
+console.log(LikesClouds);
+
+
+let str = "Hello World!"
+let iDontKnow;
+let star = "Bye Bye";
+console.log(typeof star);
+console.log(typeof str);
+console.log(typeof iDontKnow);
+
+
+
+const READLINE = require("readline-sync")
+let userInput = READLINE.question("Hey there!");
+console.log(userInput);
+console.log(require);
