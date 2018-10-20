07d5497a0b8b02b30249432bad5d9944510d2bff
Talia Campbell <talia.campbell@csedge.org>
2018-10-20 16:03:08 -0400

    Finished Change Machine

--- change.js
+++ change.js
@@ -1,7 +1,53 @@
-// import readline-sync module, don't touch!
+// import readline-sync module, don't touch
 const READLINE = require("readline-sync");
 
-// fill in object below with all relevant properties (see instructions)
-let changeMachine = {};
+let changeMachine = {
+	inputCents: undefined,
+	quarters: undefined,
+	nickles: undefined,
+	dimes: undefined,
+	pennies: undefined,
+	greeting: "Welcome! Please enter the amount of coins!: "
+	farewell: "Thank you, come again"
+};
+changeMachine.inputCents = READLINE.question(changeMachine.greeting)
 
-// write code below that simulates the change machine in action!
+let cents = changeMachine.inputCents;
+
+let quarters = Math.floor(cents/25);
+let remainder = cents%25;
+let dimes = Math.floor(remainder / 10); 
+remainder = remainder % 10;
+let nickles = Math.floor(remainder / 5);
+remainder = remainder % 5;
+let pennies = remainder / 1;
+
+changeMachine.quarters = quarters;
+changeMachine.dimes = dimes; 
+changeMachine.nickles = nickles; 
+changeMachine.pennies = pennies;
+
+console.log(`${changeMachine.inputCents} makes: 
+	${changeMachine.quarters} quarters
+	${changeMachine.nickles} nickles
+	${changeMachine.pennies} pennies
+	${changeMachine.dimes} dimes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ object.js
@@ -0,0 +1,37 @@
+// let spy = {
+// 	firstName: "James",
+// 	isSpy: true,
+// 	lastName: "Bond"
+
+// };
+
+
+let villain = {
+	first: "Shau",
+	isSpy: true,
+	last: "Maucker",
+	catchphrase: "It isn't 'Shaw Mawker'..It's 'Shoe Mocker'",
+	Crime: "knocked out the power in all of the world for one month"
+};
+
+console.log(`The villain goes by ${villain.first} ${villain.last}`)
+console.log(`Shau's famous line is "${villain.catchphrase}"`)
+console.log(`His well known crime was when he ${villain.Crime}`)
+
+
+  let hero = {
+  	first: "Marley",
+  	isSpy: true, 
+ 	last: "Parley",
+   	nickname: "Molly",
+  	catchphrase: "I like big macs",
+  	archenemy: "Shau Maucker"
+
+  };
+  console.log(`The famous hacker ${hero.first} ${hero.last} is here,`)
+  console.log(`She always likes to say "${hero.catchphrase}."`)
+   console.log(`Her biggest enemy is Shau Maucker and she plans to defeat him`)
+
+
+
+
