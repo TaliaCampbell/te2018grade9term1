1b3d9ec14041b38772799de5317df3d99e392d74
Talia Campbell <talia.campbell@csedge.org>
2018-11-17 12:38:49 -0800

    Update haunted.js

--- haunted.js
+++ haunted.js
@@ -1,74 +1,5 @@
-//const READLINE = require("readline-sync");
 
-// let player = {
-//   name: undefined,
-//   courage: 0,
-//   intellect: 0,
-//   strength: 0,
-//   charm: 0,
-//   weapon: "fists",
-//   item: "flashlight",
-// };
-
-// console.log();
-// console.log("*** WELCOME TO THE HAUNTED HOUSE ***");
-// player.name = READLINE.question("What is your name?: ");
-
-// console.log(`${player.name} stands in front of a spooky haunted house.`);
-// console.log(`${player.name} hears what sounds like screaming coming from inside the house.`);
-// let enterHouse = READLINE.question("Will you enter the house? (yes or no) ");
-// if(enterHouse == "y" || enterHouse == "yes") {
-//   player.courage++;
-//   console.log(`${player.name} prepares their ${player.weapon} and opens the door...`);
-//   // continue the story
-
-// } else {
-//   player.intellect++;
-//   console.log(`${player.name} decides not to open the door. However...`);
-//   // continue the story
-
-// }
-// console.log("Thanks for playing!");
-
-// const READLINE = require("readline-sync");
-// console.log("⇨⇨⇨Before we start, in order to answer the questions and get the correct response you must say what is in **. What ever is in ** is one of the 2 answers you can choose to get your response.⇦⇦⇦")
-// console.log("★Welcome to the famous spooky haunted house. This house has been said to be haunted since 1876. Anyone who entered has never made it out...alive.★");
-// let enterHouse = READLINE.question("Do you dare to the house?");
-// if(enterHouse == "yes") {
-//   console.log("You are officially in side the house, watch out for spiders, they haven’t eaten in a while.");
-// }
-// else {
-//   console.log("No? I don't take no for an answer you are going in anyways.You are officially in side the house, watch out for spiders, they haven’t eaten in a while.")
-// }
-
-// let leftRight = READLINE.question("There are two doors, will you enter the left or the right?")
-// if(leftRight == "left"){
-//   console.log("Congrats you have made it to the next door..Watch your step, there are mouse traps all over the floor. “BOOM” the door behind you slams shut!")
-// }
-
-//   let openWalk = READLINE.question("Will you try to **open the door** or will you **walk across** to the next door?")
-//   if(openWalk == "walk across"){
-//     console.log("You made it across safely but you found the stairs. Will you go upstairs or stay down?")
-// }
-// if(openWalk == "open the door"){
-//   console.log("The door is completely jammed you have no option but to walk across...You made it safe but found a staircase")
-// }
-  
-//   console.log("Woah watch out! There are blood sucking bats what should you do? ")
-//   let runFight = READLINE.question("Will you run or fight?")
-//   if(runFight == "run" || runFight == "Run"){
-//     console.log("Congrats you have made it to the next door..Watch your step, there are mouse traps all over the floor. “BOOM” the door behind you slams shut!")
-//   }
-// if(runFight == "fight" || runFight == "Fight"){
-//     console.log("There were too many baths to fight off, luckily you made it out of the house with bruises and bitemarks. You have failed...")  // let walkOpen READLINE.q
-//   }
-
-
-// if(openWalk == "open the door"){
-//   console.log("The door is completely jammed you have no option but to walk across...You made it safe but found a staircase")
-// }
-
-  const READLINE = require("readline-sync")
+35  const READLINE = require("readline-sync")
  console.log("★Welcome to the famous spooky haunted house. This house has been said to be haunted since 1876. Anyone who entered has never made it out...alive.★");
 
 let enterHouse = READLINE.question("Will you enter the haunted house?");
@@ -133,5 +64,5 @@ if(runSell == "Run" || runSell == "run"){
 console.log("*****************************************************************")
 console.log("**************************The End********************************")
 console.log("*****************************************************************")
-.
+
 
